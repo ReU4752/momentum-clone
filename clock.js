81ddcc7d7f4a4fbacf119d593affd0dc511f64f1
ReU4752 <liyu4752@gmail.com>
2020-03-06 03:01:19 +0900

    Update clock
    I added meridiem function and maked hours show 12-hour clock.

--- clock.js
+++ clock.js
@@ -1,16 +1,27 @@
 const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
+  clockTitle = clockContainer.querySelector("h1"),
+  meridiem = clockContainer.querySelector("h2");
+
+function isMoring(hours) {
+  return hours < 12 ? true : false;
+}
+
+function make12Hours(hours) {
+  return hours > 12 ? hours - 12 : hours;
+}
 
 function getTime() {
   const date = new Date();
 
-  const hours = date.getHours();
+  const hours = make12Hours(date.getHours());
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
 
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+  meridiem.innerText = `${isMoring(hours) == true ? "am" : "pm"}`;
 }
 
 function init() {
